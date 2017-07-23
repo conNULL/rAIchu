8886502b97186d7b97d1fd1a4261a7942096d435
Connal de Souza <connaldesouza96@gmail.com>
2017-07-23 19:11:44 -0400

    Client automatically starts a random battle and opens the appropriate page in a web browser

--- Client.py
+++ Client.py
@@ -5,9 +5,18 @@ import time
 import _thread
 
 
-#webbrowser.open("https://play.pokemonshowdown.com/")
 def on_message(ws, message):
-    print(message)
+    global in_battle
+    global battle_tag
+    
+    if not message == '':
+        if not in_battle:
+            response = message.split(battle_tag)
+            battle_tag += response[1].split('\n')[0]
+            webbrowser.open("https://play.pokemonshowdown.com/" + battle_tag)
+            in_battle = True
+        
+        print(message)
 
 def on_error(ws, error):
     print(error)
@@ -16,11 +25,17 @@ def on_close(ws):
     print("### closed ###")
 
 def on_open(ws):
+    
+    global in_battle
+    
     def run(*args):
+        while not in_battle:
+            time.sleep(1)
+            ws.send('|/battle!')
         for i in range(100):
             time.sleep(1)
-            command = input()
-            ws.send(command)
+            command = input('|\/'+ battle_tag + ' : ')
+            ws.send(battle_tag + '|/' + command)
         time.sleep(1)
         ws.close()
         print("thread terminating...")
@@ -35,5 +50,6 @@ if __name__ == "__main__":
                               on_error = on_error,
                               on_close = on_close)
     ws.on_open = on_open
+    in_battle = False
+    battle_tag = "battle-gen7randombattle-"
     ws.run_forever()
-    tag = "battle-gen7randombattle-"
