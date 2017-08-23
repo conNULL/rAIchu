81b270338ead27aeb2eb3e00f82e2e94656bb481
Connal de Souza <connaldesouza96@gmail.com>
2017-08-23 19:08:48 -0400

    Improved modularity of code by adding Battle class. Preparation to support multiple simultaneous battles.

--- Client.py
+++ Client.py
@@ -12,87 +12,128 @@ class MoveType(Enum):
     NONE = 0
     BATTLE_ACTION = 1
     BATTLE_SWITCH = 2
-
-
-def make_move(ws, battle_info):
-
-    global manual
-    global random_move
     
-    if manual:
-        command = input('|\/'+ battle_tag + ' : ')
-    elif random_move:
-        moves = generate_moves(battle_info)
-        command = moves[random.randint(0, len(moves)-1)]
+class AIType(Enum):
+    RANDOM = 0
+    MANUAL = 1
     
-    return command
-def generate_moves(battle_info):
-    global move_required
     
-    moves = []
+class Battle():
+    
 
-    if move_required == MoveType.BATTLE_ACTION or move_required == MoveType.BATTLE_SWITCH:
+    def __init__(self, id):
+        self.id = id
+        self.info = {}
+        self.move_required = MoveType.BATTLE_ACTION
+        webbrowser.open("https://play.pokemonshowdown.com/" + Battle.tag + id)
         
-        for i in range(len(battle_info['pokemon'])):
-            if battle_info['pokemon'][i]['active'] == False and battle_info['pokemon'][i]['condition'] != '0 fnt':
-                moves.append('switch ' + str(i+1))
-    if move_required == MoveType.BATTLE_ACTION:
-        for i in range(4):
-            if battle_info['active']['moves'][i]['pp'] > 0 and not battle_info['active']['moves'][i]['disabled']:
-                moves.append('move ' + str(i+1))
-            
-    return moves
-            
+    def initialize(web_socket, ai, tag):
+        Battle.ws = web_socket
+        Battle.ai = ai
+        Battle.tag = tag
 
-def update_battle_info(message):
-    global battle_info
-    global move_required
 
-    if 'moves' in message and 'baseAbility' in message:
-        json_message = json.loads(message.split('request|')[1])
-        battle_info['pokemon'] =  json_message['side']['pokemon']
-        battle_info['active'] = json_message['active'][0]
-        battle_info['id'] = battle_info['pokemon'][0]['ident'][1]
-
-    if '|turn|' in message:
-        move_required = MoveType.BATTLE_ACTION
-    if '|faint|' in message:
-        print('-------------------------------------')
-        print(message)
-        print('-------------------------------------')
-        id = message.split('faint|p')[1].split('a')[0]
-        if id == battle_info['id']:
-            move_required = MoveType.BATTLE_SWITCH
-            pokemon = message.split('faint|')[1].split('|0 fnt')[0]
-            ident = pokemon[:2] + pokemon[3:]
-            print(ident)
-            for i in range(6):    
-                if battle_info['pokemon'][i]['ident'] == ident:
-                    battle_info['pokemon'][i]['condition'] = '0 fnt'
+    def make_move(self):
         
+        if self.move_required == MoveType.NONE:
+            return
+            
+        if self.ai == AIType.MANUAL:
+            command = input('|/'+ Battle.tag + self.id + ' : ')
+        elif self.ai == AIType.RANDOM:
+            moves = self.generate_moves()
+            command = moves[random.randint(0, len(moves)-1)]
+        
+        command = Battle.tag + self.id + '|/'+ command
+        Battle.ws.send(command)
+        print('COMMAND', command)
+        self.move_required == MoveType.NONE
+        
+    def generate_moves(self):
+        
+        moves = []
     
+        if self.move_required == MoveType.BATTLE_ACTION or self.move_required == MoveType.BATTLE_SWITCH:
+            
+            for i in range(len(self.info['pokemon'])):
+                if self.info['pokemon'][i]['active'] == False and self.info['pokemon'][i]['condition'] != '0 fnt':
+                    moves.append('switch ' + str(i+1))
+        if self.move_required == MoveType.BATTLE_ACTION:
+            for i in range(4):
+                if self.info['active']['moves'][i]['pp'] > 0 and not self.info['active']['moves'][i]['disabled']:
+                    moves.append('move ' + str(i+1))
+                
+        return moves
+            
+
+    def update_battle_info(self, message):
+       
+        if 'moves' in message and 'baseAbility' in message:
+            json_message = json.loads(message.split('request|')[1])
+            self.info['pokemon'] =  json_message['side']['pokemon']
+            self.info['active'] = json_message['active'][0]
+            self.info['id'] = self.info['pokemon'][0]['ident'][1]
+    
+        if '|turn|' in message:
+            print('turn-----------')
+            self.move_required = MoveType.BATTLE_ACTION
+        if '|faint|' in message:
+            id = message.split('faint|p')[1].split('a')[0]
+            if id == self.info['id']:
+                self.move_required = MoveType.BATTLE_SWITCH
+                pokemon = message.split('faint|')[1].split('|0 fnt')[0]
+                ident = pokemon[:2] + pokemon[3:]
+                print(ident)
+                for i in range(6):    
+                    if self.info['pokemon'][i]['ident'] == ident:
+                        self.info['pokemon'][i]['condition'] = '0 fnt'
+        self.make_move()
+    def reset_battle_info(self):
+        
+        self.battle_info = {}
+            
 def on_message(ws, message):
     global in_battle
     global battle_tag
     global move_required
+    global username
+    global logged_in
+    global battles
     
-
-    print(message)
-    if '|challstr|' in message:
+    
+    
+  
+    if logged_in:
+            
+        battle_tag = message.split('\n')[0]
+        battle_id = battle_tag.split(Battle.tag)[1]
+        print('BATTLE:', battle_id)
+        
+        if battle_id not in battles.keys():
+            
+            new_battle = Battle(battle_id)        
+            battles[battle_id] = new_battle
+            
+        elif '|choice|move batonpass|' in message or '|choice||move uturn' in message:
+            battles[battle_id].move_required = MoveType.BATTLE_SWITCH
+            
+        elif (username + '\'s rating:') in message and battle_tag in message:
+            ws.send('|/leave ' + battle_tag)
+            del battles[battle_id]
+            ws.send('|/battle!')
+        
+        battles[battle_id].update_battle_info(message)
+            
+    elif '|challstr|' in message:
         challstr = message[10:]
         print(challstr)
-        login(challstr, ws)
-    elif not in_battle:
-        response = message.split(battle_tag)
-        battle_tag += response[1].split('\n')[0]
-        webbrowser.open("https://play.pokemonshowdown.com/" + battle_tag)
-        in_battle = True
-        move_required = MoveType.BATTLE_ACTION
-
-    else:
-        update_battle_info(message)
+        logged_in = login(challstr, ws)
+        
+    print(message)
 
 def login(challstr, ws):
+    
+    global username
     
     f = open('login.txt', 'r').readlines()
     username = f[0][:-1]
@@ -102,7 +143,9 @@ def login(challstr, ws):
     print(r.status_code, r.reason)
     assertion = r.text.split('"assertion":"')[1][:-2]
     ws.send('|/trn ' + username + ',0,' + assertion)
-    ws.send('|/battle!')
+    if r.status_code == 200:
+        return True
+    return False
     
 def on_error(ws, error):
     print(error)
@@ -118,37 +161,48 @@ def on_open(ws):
         global in_battle
         global battle_info
         global move_required
+        global NUM_BATTLES
+        global logged_in
         
-        while not in_battle:
+        logged_in = False
+        
+        while not logged_in:
             time.sleep(1)
-        for i in range(100):
-            time.sleep(10)
-            if move_required != MoveType.NONE:
-                command = make_move(ws, battle_info)
-                ws.send(battle_tag + '|/' + command)
-                move_required = MoveType.NONE
+         
+        for i in range(NUM_BATTLES):
+            time.sleep(5)
+            ws.send('|/battle!')
+            
+        for i in range(100000):
+            
+            
+                
+            time.sleep(1)
+            
         time.sleep(1)
         ws.close()
         print("thread terminating...")
     _thread.start_new_thread(run, ())
 
 
+    
 if __name__ == "__main__":
-    global battle_info
     global random_move
     global manual
+    global battles
     
     manual = False
     random_move = True
+    NUM_BATTLES = 1
+    TAG = 'battle-gen7randombattle-'
+    AI = AIType.RANDOM
     
     websocket.enableTrace(True)
     ws = websocket.WebSocketApp("wss://sim2.psim.us/showdown/websocket",
-    #ws = websocket.WebSocketApp("ws://sim.smogon.com:8000/showdown/websocket",
                               on_message = on_message,
                               on_error = on_error,
                               on_close = on_close)
     ws.on_open = on_open
-    in_battle = False
-    battle_info = {}
-    battle_tag = "battle-gen7randombattle-"
+    battles = {}
+    Battle.initialize(ws, AI,TAG)
     ws.run_forever()
