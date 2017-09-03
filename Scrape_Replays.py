6bfe3188dc4d591741988884563b3f7f9a6b61e9
Connal de Souza <connaldesouza96@gmail.com>
2017-09-03 03:59:57 -0400

    Added scrape search option that does periodic checks for recent replay ids available on the website.

--- Scrape_Replays.py
+++ Scrape_Replays.py
@@ -2,10 +2,16 @@ from pyquery import PyQuery as pq
 import os
 import requests
 from enum import Enum
+import time
 
-class Direction(Enum):
-    UP = 0
-    DOWN = 1
+class SearchType(Enum):
+    
+    #Can save a lot but very slowly
+    LINEAR_UP = 0
+    LINEAR_DOWN = 1
+    
+    #Saves 50 replays very quickly (some ids are available on the website)
+    RECENT = 2
     
 def save_replay_text(battle_id, base_url, base_ofile):
     
@@ -34,26 +40,64 @@ def save_replay_text(battle_id, base_url, base_ofile):
 BASE_URL = 'https://replay.pokemonshowdown.com/gen7randombattle-'
 OUTPUT_FILE_BASE = 'REPLAY_RAW_'
 OUTPUT_DIRECTORY = 'gen_7_random_battle_replays'
-START_BATTLE_ID = 624883091
-DIRECTION = Direction.DOWN
-NUM_TO_SAVE = 100
+
+#Starting point for linear searches
+START_BATTLE_ID = 624870638
+
+#Recent or Linear
+TYPE = SearchType.RECENT
+
+NUM_TO_SAVE = 10 #minimum
+
+#Time to wait before doing recent check again
+WAIT_TIME = 90 #15 minutes
+
+#Maximum number of ids to try in a linear search
+MAX_TO_CHECK = 10000
+
 REPLAYS_TO_SAVE = 100
 
 if not os.path.exists(OUTPUT_DIRECTORY):
     os.mkdir(OUTPUT_DIRECTORY)
-    
-if DIRECTION == Direction.UP:
-    inc = 1
-else:
-    inc = -1
-    
+
+        
 saved = 0
-replay_id = START_BATTLE_ID
-while saved < NUM_TO_SAVE:
-    
-    if save_replay_text(replay_id, BASE_URL, OUTPUT_DIRECTORY+'/'+OUTPUT_FILE_BASE):
-        saved += 1
-    replay_id += inc
-    
+if TYPE == SearchType.LINEAR_UP or TYPE == SearchType.LINEAR_DOWN:
+    if TYPE == SearchType.LINEAR_UP:
+        inc = 1
+    else:
+        inc = -1
+        
+    tried = 0
+    replay_id = START_BATTLE_ID
+    while saved < NUM_TO_SAVE and tried < MAX_TO_CHECK:
+        
+        tried += 1
+        if save_replay_text(replay_id, BASE_URL, OUTPUT_DIRECTORY+'/'+OUTPUT_FILE_BASE):
+            saved += 1
+        replay_id += inc
+ 
+elif TYPE == SearchType.RECENT:
+    done = False
+    while not done:
+        args = {'output': 'html', 'format' : 'gen7randombattle'}
+        html = requests.get('https://replay.pokemonshowdown.com/search/', params=args)
+        
+        ids = [k[:9] for k in html.text.split('href="/gen7randombattle-')[1:]]
+        
+        for replay_id in ids:
+            if save_replay_text(replay_id, BASE_URL, OUTPUT_DIRECTORY+'/'+OUTPUT_FILE_BASE):
+                saved += 1
+            else:
+                replay_id = ids[saved-1]
+                break
+        #Only wait if there more replays need to be saved.
+        if saved < NUM_TO_SAVE:
+            time.sleep(WAIT_TIME)
+        else:
+            done = True
+        
+            
+        
 print('done! Last Saved Replay: ', replay_id)
 #update the START_BATTLE_ID when finished to continue next time. 
